223aeac741267d2b12672e96f2a2aad4c71b6adc
DNTt30 <duongngoctund2004@gmail.com>
2026-06-02 22:48:01 +0700

    Fix AdminApiController and update history.html UI

--- gs-serving-web-content-main/complete/src/main/java/com/duong/salesmanagement/controller/AdminApiController.java
+++ gs-serving-web-content-main/complete/src/main/java/com/duong/salesmanagement/controller/AdminApiController.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RestController
